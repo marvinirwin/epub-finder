e398753795e2aa9eb91b5d81fc7381a016046cba
Marvin Irwin <marvin@marvinirwin.com>
2020-05-10 19:36:12 -0700

    first commit, reader working, vocab ok, I think the finder is not great

---
+++ reader/src/App.tsx
@@ -0,0 +1,110 @@
+import './declaration.d';
+import 'jquery-ui-bundle';
+import 'jquery-ui-bundle/jquery-ui.css';
+import $ from 'jquery';
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Epub from 'epubjs';
+import vocab from './hsk.json';
+import Rendition from "epubjs/types/rendition";
+// @ts-ignore
+import {sify} from 'chinese-conv';
+
+// export for others scripts to use
+// @ts-ignore
+window.$ = $;
+
+interface Vocab {
+    id: number;
+    hanzi: string;
+    pinyin: string;
+    translations: string[]
+}
+interface VocabMap {
+    [key: string]: Vocab;
+}
+function makeVocab(hanzi: string, pinyin: string, ...translations: string[]): Vocab {
+    return {
+        id: 0,
+        hanzi,
+        pinyin,
+        translations
+    }
+}
+// import * as ranges from './ranges.json';
+const v: Vocab[] = vocab;
+const myVocab: Vocab[] = [
+    makeVocab('却', 'qu4', 'however', 'yet'),
+    makeVocab('仙', 'xien1', 'magical')
+    ]
+;
+const vocabMap: VocabMap = v.concat(myVocab).reduce((acc: VocabMap, v: Vocab) => {
+    acc[v.hanzi.normalize()] = v;
+    return acc;
+}, {})
+
+
+
+let book = Epub("pg23962.epub", {});
+
+
+function App() {
+    const [b, setB] = useState();
+    useEffect(() => {
+        (async () => {
+            await book.ready;
+            let elementById = document.getElementById('book');
+            if (elementById) {
+                const rendition = book.renderTo(elementById, {width: 600, height: 400})
+                let target = '#id00707';
+                rendition.display(target).then(() => {
+                    const parent = $('iframe').contents().find(target);
+                    parent.parent().append(`
+                    <style>
+.hoverable {
+  background-color: lightyellow;
+}
+.hoverable:hover {
+  background-color: lightgreen;
+}
+</style>
+                    `)
+                    const words = parent.text().normalize().split('');
+                    const root = $('<div/>');
+                    for (let i = 0; i < words.length; i++) {
+                        const word = sify(words[i]);
+                        const el = $('<span/>');
+                        el.text(word);
+                        let t = vocabMap[word];
+                        if (t) {
+                            el.addClass('hoverable')
+                            // @ts-ignore
+                            el.attr('title',
+                            `
+                            ${t.pinyin} -
+                            ${t.translations.join(', ')}
+                            
+                            `
+                                );
+                            // @ts-ignore
+                            el.tooltip();
+                        }
+                        root.append(el);
+                    }
+                    parent.text('');
+                    parent.append(root);
+                });
+
+            }
+            setB(book)
+        })()
+    }, []);
+    return (
+        <div style={{position: "relative", height: "100vh", width: '100vw'}}>
+            {" "}
+                <div id="book"/>
+        </div>
+    );
+}
+
+export default App;
