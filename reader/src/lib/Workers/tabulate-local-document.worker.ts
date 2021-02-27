d6e95f05e1cf41f797fa586b57b7a91d22e6bdc2
Marvin Irwin <marvin@marvinirwin.com>
2021-02-27 08:51:44 -0800

    call stack error gone

--- reader/src/lib/Workers/tabulate-local-document.worker.ts
+++ reader/src/lib/Workers/tabulate-local-document.worker.ts
@@ -1,8 +1,8 @@
 /* eslint no-restricted-globals: 0 */
 // @ts-ignore
 // noinspection JSConstantReassignment
-import {AtomizedDocument} from "../../../../server/src/shared/tabulate-documents/atomized-document";
-import {Segment, tabulatedSentenceToTabulatedDocuments} from "@shared/";
+import {AtomizedDocument} from "@shared/";
+import {Segment, SerializedTabulation, tabulatedSentenceToTabulatedDocuments} from "@shared/";
 import trie from "trie-prefix-tree";
 import {TabulateLocalDocumentDto} from "./tabulate-local-document.dto";
 
@@ -14,10 +14,31 @@ const ctx: Worker = self as any;
 ctx.onmessage = async (ev) => {
     const {trieWords, src, label}: TabulateLocalDocumentDto = ev.data;
     const doc = AtomizedDocument.atomizeDocument(src);
-    const tabulated = tabulatedSentenceToTabulatedDocuments(Segment.tabulate(
-        trie(trieWords),
-        doc.segments(),
-    ), label);
-    ctx.postMessage(tabulated);
+    const t = trie(trieWords);
+    const segments = doc.segments();
+    const tabulatedSentences = Segment.tabulate(
+        t,
+        segments,
+    );
+    try {
+        const tabulated = tabulatedSentenceToTabulatedDocuments(tabulatedSentences, label);
+        ctx.postMessage({
+            wordCounts: tabulated.wordCounts,
+            wordSegmentStringsMap: tabulated.wordSegmentStringsMap,
+        } as SerializedTabulation);
+    } catch(e) {
+        console.error(e);
+        ctx.postMessage(
+            {
+                documentWordCounts: {},
+                wordElementsMap: {},
+                wordSegmentMap: {},
+                segments: [],
+                atomMetadatas: new Map(),
+                wordCounts: {},
+                wordSegmentStringsMap: new Map()
+            }
+        )
+    }
 };
 
