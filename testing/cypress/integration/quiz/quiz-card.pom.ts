f53e172d07c7233e65d97ccba354b939cd25cf2b
Marvin Irwin <marvin@marvinirwin.com>
2021-03-05 13:28:10 -0800

    quiz card test more fleshed out

--- testing/cypress/integration/quiz/quiz-card.pom.ts
+++ testing/cypress/integration/quiz/quiz-card.pom.ts
@@ -6,55 +6,5 @@ export class QuizCardPom {
     constructor(private id: string) {
     }
 
-    body() {
-        return cy.get(this.id);
-    }
-
-    img() {
-        this.body().find('img')
-    }
-
-    easy() {
-        return this.body().find('.quiz-button-easy')
-    }
-
-    medium() {
-        return this.body().find('.quiz-button-medium')
-    }
-
-    hard() {
-        return this.body().find('.quiz-button-hard')
-    }
-
-    hide() {
-        return this.body().find('.quiz-button-hide')
-    }
-
-    characters() {
-        return this.body().find('.quiz-text')
-    }
-
-    exampleSentences() {
-        return this.body()
-            .find('iframe')
-            .iframeBody()
-            .find('.example-sentence')
-    }
-
-    editDescription(newDescription: string) {
-        this.body()
-            .find('.known-language')
-            .type(newDescription);
-    }
-
-    selectNewImage() {
-        // HACK, I just don't want to verify what src there is not, I'm just happy if it's not empty
-        const oldSrc = '';
-        ImageSearchPom.SelectFirstSearchResult();
-        // Now assert we have an image we clicked (Or since I'm lazy, just not the previous one
-        this.body()
-            .find('.image')
-            .should('have.attr', 'src').should('not.include', oldSrc);
-    }
 
 }
--- testing/cypress/integration/quiz/quiz-card.spec.ts
+++ testing/cypress/integration/quiz/quiz-card.spec.ts
@@ -2,6 +2,7 @@
 import {CardList} from "./quiz.document";
 import {DirectoryPom} from "../../support/pom/directory.pom";
 import {QuizCardPom} from "./quiz-card.pom";
+import {ImageSearchPom} from "./image-search.pom";
 
 const CurrentQuizCard = '#current-quiz-card';
 
@@ -17,7 +18,58 @@ const defaultHotkeys = {
 
 class QuizCarouselPom {
     static goToQuizCard(word: string) {
+        // Probably some sort of while loop with promises
+    }
 
+    body() {
+        return cy.get(this.id);
+    }
+
+    img() {
+        cy.find('img')
+    }
+
+    easy() {
+        return cy.find('.quiz-button-easy')
+    }
+
+    medium() {
+        return cy.find('.quiz-button-medium')
+    }
+
+    hard() {
+        return cy.find('.quiz-button-hard')
+    }
+
+    hide() {
+        return cy.find('.quiz-button-hide')
+    }
+
+    characters() {
+        return cy.find('.quiz-text')
+    }
+
+    exampleSentences() {
+        return cy
+            .find('iframe')
+            .iframeBody()
+            .find('.example-sentence')
+    }
+
+    editDescription(newDescription: string) {
+        cy
+            .find('.known-language')
+            .type(newDescription);
+    }
+
+    selectNewImage() {
+        // HACK, I just don't want to verify what src there is not, I'm just happy if it's not empty
+        const oldSrc = '';
+        ImageSearchPom.SelectFirstSearchResult();
+        // Now assert we have an image we clicked (Or since I'm lazy, just not the previous one
+        cy
+            .find('.image')
+            .should('have.attr', 'src').should('not.include', oldSrc);
     }
 }
 
@@ -26,17 +78,30 @@ describe('Quiz Cards', () => {
     beforeEach(() => {
     })
     it('Shows the correct card body', () => {
-        DirectoryPom.goToQuiz();
-        const card = new QuizCardPom(CurrentQuizCard);
         const firstCard = CardList[0];
-        card.characters().should('contain', firstCard);
-        card.editDescription('test');
-        card.selectNewImage();
+        DirectoryPom.goToQuiz();
+        QuizCarouselPom.setHiddenFields('hiddenDefinition');
+        // Assert the definition and description are hidden
+        QuizCarouselPom.definitionTextShouldBe('')
+        QuizCarouselPom.descriptionTextShouldBe('')
+
+        QuizCarouselPom.setHiddenFields('hiddenLearningLanguage');
+        // Assert learning language empty
+        QuizCarouselPom.learningLanguageTextShouldBe('');
+
+        // Now reveal the while card
+        QuizCarouselPom.reveal();
+        QuizCarouselPom.definitionTextShouldBe(firstCard.description);
+        QuizCarouselPom.editDescription('test');
+        QuizCarouselPom.descriptionTextShouldBe(firstCard.description);
+        QuizCarouselPom.learningLanguageTextShouldBe(firstCard.characters);
+        QuizCarouselPom.selectNewImage();
+        QuizCarouselPom.image().should('not.be.empty')
         DirectoryPom.pressHotkey(defaultHotkeys.quizScore5);
         // Assert the word is different
         // Now how do we get back to the original quiz card?
-        QuizCarouselPom.goToQuizCard();
-
+        QuizCarouselPom.learningLanguageTextShouldBe('');
+        QuizCarouselPom.goToQuizCard(firstCard.characters);
     });
 })
 
