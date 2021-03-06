a2667c1eef1a36dfa0f0fc8db814fd9870a5811b
Marvin Irwin <marvin@marvinirwin.com>
2021-03-06 13:27:49 -0800

    hidden fields

---
+++ server/src/revision-updater.ts
@@ -0,0 +1,11 @@
+export class RevisionUpdater<T, RevisionT extends Partial<T>> {
+    constructor(
+        isAllowedFn: (currentVersion: T) => Promise<boolean>
+    ) {
+    }
+    public static SubmitRevision(
+
+    ) {
+
+    }
+}
