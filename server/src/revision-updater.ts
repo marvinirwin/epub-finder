7d7e2e0c82e03ac190aaa1acfc79f51afc1fc10b
Marvin Irwin <marvin@marvinirwin.com>
2021-03-06 13:35:08 -0800

    revision submitted

--- server/src/revision-updater.ts
+++ server/src/revision-updater.ts
@@ -1,11 +1,22 @@
 export class RevisionUpdater<T, RevisionT extends Partial<T>> {
     constructor(
-        isAllowedFn: (currentVersion: T) => Promise<boolean>
+        private getCurrentVersion: (newRevision: RevisionT) => Promise<T>,
+        private isAllowedFn: (currentVersion: T) => Promise<boolean>,
+        private mergeVersions: (currentVersion: T, newRevision: RevisionT) => T,
+        private persistNewVersion: (newVersion: T) => Promise<T>
     ) {
     }
-    public static SubmitRevision(
+    public async SubmitRevision(
+        newRevision: RevisionT
+    ): Promise<T> {
+        const currentVersion = await this.getCurrentVersion(newRevision);
+        if (!currentVersion) {
+            throw new Error("Cannot find current version");
+        }
+        if (!await (this.isAllowedFn(currentVersion))) {
+            throw new Error("Not authorized to submit revision")
+        }
 
-    ) {
-
+        return await this.persistNewVersion(await this.mergeVersions(currentVersion, newRevision))
     }
 }
