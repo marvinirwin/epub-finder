bdb54a71c80ac894ce7a9c9a03cace09420d2ddb
Marvin Irwin <marvin@marvinirwin.com>
2021-04-29 13:28:40 -0700

    word grid

--- reader/src/components/app-directory/nodes/reading-progress.node.tsx
+++ reader/src/components/app-directory/nodes/reading-progress.node.tsx
@@ -1,16 +1,32 @@
 import { TreeMenuNode } from '../tree-menu-node.interface'
 import React, { useContext } from 'react'
-import {  READING_PROGRESS_NODE } from '@shared/'
+import { READING_PROGRESS_NODE } from '@shared/'
 import { TrendingUp } from '@material-ui/icons'
 import { ManagerContext } from '../../../App'
 import { Box, Paper, Typography } from '@material-ui/core'
 import { useObservableState } from 'observable-hooks'
 import { Manager } from '../../../lib/manager/Manager'
+import Button from '@material-ui/core/Button'
 
+export const WordGrid: React.FC<{ words: string[] }> = ({ words }) => {
+    const m = useContext(ManagerContext)
+    return <Box m={2} p={1}>
+        <Paper style={{ display: 'flex', flexFlow: 'row wrap' }}>
+            {
+                words.map(word => <Button
+                    key={word}
+                    onClick={() => m.wordCardModalService.word$.next(word)}>
+                    {word}
+                </Button>)
+            }
+        </Paper>
+    </Box>
+}
 export const ReadingProgress = () => {
     const m = useContext(ManagerContext)
-    const readingProgress = useObservableState(m.readingProgressService.readingProgressRecords$)
-    return <Box m={2} p={1} style={{width: '90vw', height: '90vh'}}>
+    const readingProgress = useObservableState(m.readingProgressService.readingProgressRecords$);
+    const learnedToday = useObservableState(m.sortedLimitedQuizScheduleRowsService.sortedLimitedScheduleRows$)?.wordsLearnedToday || [];
+    return <Box m={2} p={1} style={{ width: '90vw', height: '90vh' }}>
         {
             readingProgress?.map(({
                                       label,
@@ -25,6 +41,7 @@ export const ReadingProgress = () => {
                 <Typography>Unknown: {unknownCount}</Typography>
             </Paper>)
         }
+        <WordGrid words={learnedToday.map(r => r.d.word)}/>
     </Box>
 }
 
