67f6b2841fcbd5d8bdc5495f0340d3287d346cdd
Marvin Irwin <marvin@marvinirwin.com>
2021-05-13 13:28:28 -0700

    onClick context soon

--- reader/src/components/quiz/todays-quiz-stats.component.tsx
+++ reader/src/components/quiz/todays-quiz-stats.component.tsx
@@ -1,12 +1,37 @@
 import React, { useContext } from 'react'
 import { ManagerContext } from '../../App'
 import { useObservableState } from 'observable-hooks'
-import { Box, Typography } from '@material-ui/core'
+import { Box, Button, Menu, MenuItem, Typography } from '@material-ui/core'
 import { quizLearnedTodayNumber, quizLearningNumber, quizToReviewNumber, quizWordsLeftForTodayNumber } from '@shared/'
 import { allScheduleRowsForWord, anyScheduleRowsForWord } from '../../lib/manager/sorted-limit-schedule-rows.service'
+import PopupState, { bindMenu, bindTrigger } from 'material-ui-popup-state'
+
+const wrapItemsWIth: React.FC<{ items: string[], onClick: (v: string) => unknown }> = ({
+                                                                                           children,
+                                                                                           items,
+                                                                                           onClick,
+                                                                                       }) => {
+    return <PopupState variant='popover' popupId='demo-popup-menu'>
+        {(popupState) => (
+            <React.Fragment>
+                <Button variant='contained' color='primary' {...bindTrigger(popupState)}>
+                    Open Menu
+                </Button>
+                <Menu {...bindMenu(popupState)}>
+                    {
+                        items.map(item => <MenuItem key={item} onClick={() => {
+                            popupState.close()
+                            onClick(item)
+                        }}>{item}</MenuItem>)
+                    }
+                </Menu>
+            </React.Fragment>
+        )}
+    </PopupState>
+}
 
 export const useScheduleInfo = () => {
-    const m = useContext(ManagerContext);
+    const m = useContext(ManagerContext)
     const scheduleInfo = useObservableState(
         m.sortedLimitedQuizScheduleRowsService.sortedLimitedScheduleRows$,
     ) || {
@@ -16,36 +41,40 @@ export const useScheduleInfo = () => {
         wordsReviewingOrLearning: [],
         wordsLeftForToday: [],
     }
-    return scheduleInfo;
+    return scheduleInfo
 }
 
 export const TodaysQuizStats = () => {
     const m = useContext(ManagerContext)
     const flashCardTypes = useObservableState(m.flashCardTypesRequiredToProgressService.activeFlashCardTypes$) || []
-    const scheduleInfo = useScheduleInfo();
+    const scheduleInfo = useScheduleInfo()
+    const learnedToday = allScheduleRowsForWord(scheduleInfo.wordsLearnedToday, flashCardTypes)
+    const wordsToReview = allScheduleRowsForWord(scheduleInfo.wordsToReview, flashCardTypes)
+    const wordsReviewingOrLearning = anyScheduleRowsForWord(scheduleInfo.wordsReviewingOrLearning, flashCardTypes)
+    const wordsLeft = allScheduleRowsForWord(scheduleInfo.wordsLeftForToday, flashCardTypes)
     return <Box m={2} p={1} className={'quiz-button-row'}>
         <Typography variant={'h6'}>
             New Words Left for Today:{' '}
             <span className={quizWordsLeftForTodayNumber}>
-                                {allScheduleRowsForWord(scheduleInfo.wordsLeftForToday, flashCardTypes).length}
+                                {wordsLeft.length}
                             </span>
         </Typography>
         <Typography variant={'h6'}>
             Being Learned:{' '}
             <span className={quizLearningNumber}>
-                                {anyScheduleRowsForWord(scheduleInfo.wordsReviewingOrLearning, flashCardTypes).length}
+                                {wordsReviewingOrLearning.length}
                             </span>
         </Typography>
         <Typography variant={'h6'}>
             To Review:{' '}
             <span className={quizToReviewNumber}>
-                                {allScheduleRowsForWord(scheduleInfo.wordsToReview, flashCardTypes).length}
+                                {wordsToReview.length}
                             </span>
         </Typography>
         <Typography variant={'h6'}>
             Learned Today:{' '}
             <span
-                className={quizLearnedTodayNumber}>{allScheduleRowsForWord(scheduleInfo.wordsLearnedToday, flashCardTypes).length}</span>
+                className={quizLearnedTodayNumber}>{learnedToday.length}</span>
         </Typography>
     </Box>
 }
