7558db578c0286982c96d6b1535b7e3238434851
Marvin Irwin <marvin@marvinirwin.com>
2021-05-16 12:58:26 -0700

    remove shared/*

--- reader/src/services/image-search.service.ts
+++ reader/src/services/image-search.service.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import { ImageObject } from '@server/'
-import { ImageSearchRequest } from '@shared/*'
 
 export type ImageSearchResult = ImageObject[];
 export const getImages = (term: string): Promise<ImageSearchResult> => {
