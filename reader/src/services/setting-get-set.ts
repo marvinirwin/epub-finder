49100382736d09ea5ee20c8aa2fce54d64faea68
Marvin Irwin <marvin@marvinirwin.com>
2021-05-25 06:28:31 -0700

    settings still annoying

--- reader/src/services/setting-get-set.ts
+++ reader/src/services/setting-get-set.ts
@@ -4,9 +4,32 @@ import { Observable, of } from 'rxjs'
 import { distinctUntilChanged, skip } from 'rxjs/operators'
 import { UserSetting } from '../../../server/src/entities/user-setting.entity'
 
-export type SettingType = 'url' | 'indexedDB' | 'REST'
+export type SettingType = 'url' | 'indexedDB' | 'REST';
+
+
+const settings = queryPersistableEntity<UserSetting>({
+    entity: 'userSettings',
+}).then(records => new Map(records.map(userSettingReceord => [userSettingReceord.name, userSettingReceord])))
 
 export class SettingGetSet<T> {
+    constructor(
+        public name: string,
+        public get: () => Promise<T> | T,
+        public set: (v: T) => Promise<void> | void,
+        private changed$: Observable<any>,
+    ) {
+        changed$
+            .pipe(
+                skip(1),
+                distinctUntilChanged((previous, after) => {
+                    return previous.href === after.href
+                }),
+            )
+            .subscribe(async (v) => {
+                this.set(await get())
+            })
+    }
+
     public static FromSettingName<SettingRepository extends SettingType, Value>(
         historyService: HistoryService,
         databaseService: DatabaseService,
@@ -19,31 +42,26 @@ export class SettingGetSet<T> {
                 return new SettingGetSet<Value>(
                     name,
                     () =>
-                        new Promise((resolve, reject) => {
-                            queryPersistableEntity<UserSetting>({
-                                entity: 'userSettings',
-                                skip: 0,
-                                take: 1,
-                                where: {
-                                    name
-                                }
-                            }).then((rows) => {
-                                const row = rows[0];
-                                if (row) {
-                                    try {
-                                        resolve(row.value)
-                                    } catch (e) {
-                                        resolve(defaultWhenNotAvailable)
-                                    }
-                                } else {
-                                    resolve(defaultWhenNotAvailable)
-                                }
-                            })
-                        }),
+                        new Promise((resolve, reject) =>
+                            settings.then(map => [map.get(name)])
+                                .then((rows) => {
+                                        const row = rows[0]
+                                        if (row) {
+                                            try {
+                                                resolve(row.value)
+                                            } catch (e) {
+                                                resolve(defaultWhenNotAvailable)
+                                            }
+                                        } else {
+                                            resolve(defaultWhenNotAvailable)
+                                        }
+                                    },
+                                ),
+                        ),
                     async (value: Value) => {
                         await putPersistableEntity<UserSetting>({
                             entity: 'userSettings',
-                            record: {name, value},
+                            record: { name, value },
                         })
                     },
                     of(),
@@ -72,23 +90,5 @@ export class SettingGetSet<T> {
             default:
                 throw new Error(`Unknown setting get/set type ${type}`)
         }
-    }
-
-    constructor(
-        public name: string,
-        public get: () => Promise<T> | T,
-        public set: (v: T) => Promise<void> | void,
-        private changed$: Observable<any>,
-    ) {
-        changed$
-            .pipe(
-                skip(1),
-                distinctUntilChanged((previous, after) => {
-                    return previous.href === after.href
-                }),
-            )
-            .subscribe(async (v) => {
-                this.set(await get())
-            })
     }
 }
