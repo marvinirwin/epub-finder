3ee04126049c662592fbc79cdb1bb8961b34fab8
Marvin Irwin <marvin@marvinirwin.com>
2021-05-29 13:19:30 -0700

    added known words to weighted index, untested

--- reader/src/lib/language/weighted-vocab.service.ts
+++ reader/src/lib/language/weighted-vocab.service.ts
@@ -1,29 +1,31 @@
-import { QuizCardScheduleRowsService } from '../schedule/quiz-card-schedule-rows.service'
-import { Observable } from 'rxjs'
+import { combineLatest, Observable } from 'rxjs'
 import { map, shareReplay } from 'rxjs/operators'
 import { WordRecognitionProgressRepository } from '../schedule/word-recognition-progress.repository'
-import {
-    recordsLearnedAnyDay,
-    wasLearnedToday,
-} from '../schedule/schedule-row'
+import { recordsLearnedAnyDay } from '../schedule/schedule-row'
+import { KnownWordsRepository } from '../schedule/known-words.repository'
 
 export class WeightedVocabService {
     weightedVocab$: Observable<Map<string, number>>
 
     constructor({
-        wordRecognitionProgressRepository,
-    }: {
+                    wordRecognitionProgressRepository,
+        knownWordsRepository
+                }: {
         wordRecognitionProgressRepository: WordRecognitionProgressRepository
+        knownWordsRepository: KnownWordsRepository
     }) {
-        this.weightedVocab$ = wordRecognitionProgressRepository.indexOfOrderedRecords$.pipe(
-            map((indexedWordRecognitionRecords) => {
-                return new Map(
-                    Object.values(indexedWordRecognitionRecords).map(
+        this.weightedVocab$ = combineLatest([
+            wordRecognitionProgressRepository.indexOfOrderedRecords$,
+            knownWordsRepository.indexOfOrderedRecords$
+        ])
+            .pipe(
+                map(([indexedWordRecognitionRecords, knownWordIndex]) => {
+                    const recognitionRowEntries: [string, number][] = Object.values(indexedWordRecognitionRecords).map(
                         (recognitionRecords) => {
                             const lastRecord =
                                 recognitionRecords[
-                                    recognitionRecords.length - 1
-                                ]
+                                recognitionRecords.length - 1
+                                    ]
                             return [
                                 lastRecord.word,
                                 recordsLearnedAnyDay(recognitionRecords)
@@ -31,10 +33,21 @@ export class WeightedVocabService {
                                     : 0,
                             ]
                         },
-                    ),
-                )
-            }),
-            shareReplay(1),
-        )
+                    );
+                    const knownWordsEntries: [string, number][] = Object.entries(knownWordIndex)
+                        .filter(([word, records]) => {
+                            const lastRecord = records[records.length - 1];
+                            return lastRecord.is_known;
+                        })
+                        .map(([word]) => [word, 1])
+                    return new Map<string, number>(
+                        [
+                            ...recognitionRowEntries,
+                            ...knownWordsEntries,
+                        ],
+                    )
+                }),
+                shareReplay(1),
+            )
     }
 }
