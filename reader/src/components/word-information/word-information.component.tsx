2e3f72fe2266802aea79d2f24691d74d727e1fa4
Marvin Irwin <marvin@marvinirwin.com>
2021-05-29 13:30:33 -0700

    updated toggle text

--- reader/src/components/word-information/word-information.component.tsx
+++ reader/src/components/word-information/word-information.component.tsx
@@ -65,7 +65,7 @@ export const WordInformationComponent: React.FC<{ wordCard: WordCard }> = ({
                             }])
                         }
                     }}>
-
+                        Is marked as known: {isKnown ? 'true' : 'false'}
                     </Button>
                 </div>
                 <br />
