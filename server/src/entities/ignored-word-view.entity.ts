1719f15f2a3a2039561ac825932a3ef339dd03f3
Marvin Irwin <marvin@marvinirwin.com>
2021-05-28 18:29:27 -0700

    ignored-word-view

---
+++ server/src/entities/ignored-word-view.entity.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, ViewColumn, ViewEntity } from 'typeorm'
+import { GroupwiseMax } from './user-setting-view.entity'
+
+@ViewEntity({
+    expression: GroupwiseMax({
+        table: 'known_word',
+        groupingColumns: [
+            'creator_id',
+            'language_code',
+            'word',
+        ]
+    }),
+})
+export class IgnoredWordView {
+    @ViewColumn()
+    id: string
+    @ViewColumn()
+    language_code: string
+    @ViewColumn()
+    word: string;
+    @ViewColumn()
+    created_at: Date;
+    @ViewColumn()
+    is_known: boolean;
+    @ViewColumn()
+    creator_id: number;
+}
