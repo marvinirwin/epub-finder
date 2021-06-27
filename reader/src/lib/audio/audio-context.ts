d8340b1574edc0416483bc35fdca14e66513b2b0
Marvin <marvin@marvinirwin.com>
2021-06-27 12:55:10 -0700

    seems to work on firefox and safari now

--- reader/src/lib/audio/audio-context.ts
+++ reader/src/lib/audio/audio-context.ts
@@ -1,6 +1,6 @@
 export const audioContext = new Promise<AudioContext>((resolve) => {
     setTimeout(() => {
         // @ts-ignore
-        resolve(new (AudioContext || window.webkitAudioContext)())
+        resolve(new (window.AudioContext || window.webkitAudioContext)())
     }, 1000)
 })
--- reader/src/lib/get-csv-description.ts
+++ reader/src/lib/get-csv-description.ts
@@ -1,24 +1,30 @@
 import {SupportedTranslation} from "../../../server/src/shared/supported-translation.service";
 import {ICard} from "../../../server/src/shared/ICard";
-import {IPositionedWord} from "../../../server/src/shared/Annotation/IPositionedWord";
 import {fetchTranslation} from "../services/translate.service";
 import {SegmentSubsequences} from "@shared/*";
 
-export async function getCsvDescription({
-                                         knownLanguage,
-                                         learningToKnowTranslationConfig,
-                                         c,
-                                         segments
-                                     }: {
-    knownLanguage: string,
-    learningToKnowTranslationConfig: SupportedTranslation | undefined,
-    c: ICard,
-    segments: SegmentSubsequences[]
-}) {
+function getSegmentTextWithWordsHighlighted(segments: SegmentSubsequences[]) {
+    return segments.map(segment => `<i>
+    ${segment.segmentText}
+    </i>`).join('<br/><br/>');
+}
+
+
+export async function getCsvDescription(
+    {
+        knownLanguage,
+        learningToKnowTranslationConfig,
+        c,
+        segments
+    }: {
+        knownLanguage: string,
+        learningToKnowTranslationConfig: SupportedTranslation | undefined,
+        c: ICard,
+        segments: SegmentSubsequences[]
+    }) {
     const definition = knownLanguage || (learningToKnowTranslationConfig ?
         await fetchTranslation({from: c.language_code, to: 'en', text: c.learning_language}) :
         '');
-    return `Definition: <b>${definition}</b><br/><br/><br/>${segments.map(segment => `<i>
-    ${segment.segmentText}
-    </i>`).join('<br/><br/>')}`;
+    const segmentTextWithWordsHighlighted = getSegmentTextWithWordsHighlighted(segments);
+    return `Definition: <b>${definition}</b><br/><br/><br/>${segmentTextWithWordsHighlighted}`;
 }
