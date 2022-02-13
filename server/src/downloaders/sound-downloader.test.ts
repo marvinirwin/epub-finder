d7ef9a8e2d36ba80a7e582e4e92f19cf722b27ab
Marvin Irwin <Marvin Irwin>
2022-02-13 08:20:53 +0000

    made tests clearer

--- server/src/downloaders/sound-downloader.test.ts
+++ server/src/downloaders/sound-downloader.test.ts
@@ -1,8 +1,12 @@
 describe("Downloading a sound file server side", () => {
   it("Downloads sound from a url and places it in the right director", () => {
-
+    // expect(fileExistsSync("The filename I want"))
   });
-  it("Downloads a transliteration of some text", () => {
-
+  it("Downloads a sound of some text", () => {
+    /**
+     * The code for this can be taken from
+     * server/src/translate/transliterate.service.ts, which does this already
+     */
+    // expect(fileExistsSync("The filename I want"))
   })
 })
