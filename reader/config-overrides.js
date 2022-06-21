e4f47285730e40d27ec2787bce0ebeb4c5ba9065
Marvin Irwin <Marvin Irwin>
2022-06-21 18:06:36 +0100

    added dev wav cache and fixed config-overrides

--- reader/config-overrides.js
+++ reader/config-overrides.js
@@ -2,6 +2,5 @@ const { alias, configPaths } = require('react-app-rewire-alias')
 module.exports = function override(config, env) {
     alias(configPaths('./tsconfig.paths.json'))(config)
     // Disable eslint-loader
-    config.module.rules.splice(1, 1)
     return config
 }
