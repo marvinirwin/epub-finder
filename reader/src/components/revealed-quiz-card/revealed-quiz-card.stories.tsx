ef235e39a83d3f46ed39a55a3260625ef3a24d7b
shinjam3 <james-shin-1998@hotmail.com>
2022-08-05 18:22:59 -0400

    added revealed-quiz-card and made some changes

--- reader/src/components/revealed-quiz-card/revealed-quiz-card.stories.tsx
+++ reader/src/components/revealed-quiz-card/revealed-quiz-card.stories.tsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React from "react";
+import { Story } from "@storybook/react";
+import { SettingsPopup } from "../settings-popup/settings-popup.component";
 import { RevealedQuizCard, RevealedQuizCardProps } from "./revealed-quiz-card.component";
 
 export default {
-  title: 'RevealedQuizCard',
+  title: "RevealedQuizCard",
   component: RevealedQuizCard,
+  parameters: {
+    docs: {
+      inlineStories: false,
+      iframeHeight: 800,
+    },
+  },
 };
 
-const Template = (args: RevealedQuizCardProps) => <RevealedQuizCard {...args} />;
+const Template: Story<RevealedQuizCardProps> = (args: RevealedQuizCardProps) => (
+  <div className="w-full h-full relative">
+    <SettingsPopup {...defaultSettingsProps} />
+    <RevealedQuizCard {...args} />
+  </div>
+);
 
 export const Default = Template.bind({});
+Default.args = {
+  progressBarPercentage: 20,
+  exampleSentences: [
+    "The quick brown fox jumps over the lazy dog",
+    "The quick brown fox jumps over the lazy dog",
+    "The quick brown fox jumps over the lazy dog",
+    "The quick brown fox jumps over the lazy dog",
+    "The quick brown fox jumps over the lazy dog",
+    "The quick brown fox jumps over the lazy dog",
+    "The quick brown fox jumps over the lazy dog",
+  ],
+  cardInfo: {
+    romanization: "Romanized",
+    knownLanguage: "Known",
+    dictionaryDefinition: "Definition",
+    sound: "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3",
+  },
+};
+
+const defaultSettingsProps = {
+  languages: [
+    {
+      value: "zh",
+      name: "Chinese",
+      variants: [
+        {
+          value: "zh-Hans",
+          name: "Chinese (Simplified)",
+        },
+        {
+          value: "zh-Hant",
+          name: "Chinese (Traditional)",
+        },
+      ],
+    },
+    {
+      value: "en",
+      name: "English",
+      variants: [
+        {
+          value: "en-CA",
+          name: "English (CA)",
+        },
+        {
+          value: "en-US",
+          name: "English (US)",
+        },
+      ],
+    },
+  ],
+  onLanguageSelect: () => {},
+  onVariantSelect: () => {},
+};
--- reader/src/components/settings-popup/settings-popup.stories.tsx
+++ reader/src/components/settings-popup/settings-popup.stories.tsx
@@ -1,30 +1,59 @@
 import React, { useState } from "react";
+import {Story} from '@storybook/react';
 import { SettingsPopup, SettingsPopupProps } from "./settings-popup.component";
 
 export default {
   title: "SettingsPopup",
   component: SettingsPopup,
+  parameters: {
+    docs: {
+      inlineStories: false,
+        iframeHeight: 500,
+    },
+  },
 };
 
-const Template = (args: SettingsPopupProps) => {
+const Template: Story<SettingsPopupProps> = (args: SettingsPopupProps) => {
   const [showSettings, setShowSettings] = useState(true);
   return (
-    <div className="w-[900px] h-[700px] relative">
-      {showSettings ? (
-        <div className="w-full h-full relative">
-          <div
-            className="absolute top-0 left-0 w-full h-full z-10 bg-[rgba(0,0,0,0.5)]"
-            onClick={() => setShowSettings(false)}
-          />
-          <SettingsPopup {...args} />
-        </div>
-      ) : (
-        <button className="border rounded-sm border-black p-3" onClick={() => setShowSettings(true)}>
-          Show Settings
-        </button>
-      )}
+    <div className="w-full h-full relative">
+      <SettingsPopup {...args} />
     </div>
   );
 };
 
 export const Default = Template.bind({});
+Default.args = {
+  languages: [
+    {
+      value: "zh",
+      name: "Chinese",
+      variants: [
+        {
+          value: "zh-Hans",
+          name: "Chinese (Simplified)",
+        },
+        {
+          value: "zh-Hant",
+          name: "Chinese (Traditional)",
+        },
+      ],
+    },
+    {
+      value: "en",
+      name: "English",
+      variants: [
+        {
+          value: "en-CA",
+          name: "English (CA)",
+        },
+        {
+          value: "en-US",
+          name: "English (US)",
+        },
+      ],
+    },
+  ],
+  onLanguageSelect: () => {},
+  onVariantSelect: () => {},
+};
