ef89e8b1116b96be2bd0480ff837547d0371c5c0
marvinirwin <marvin@marvinirwin.com>
2023-02-16 22:37:08 +0800

    added flowbite components

--- reader/src/components/library/library.component.tsx
+++ reader/src/components/library/library.component.tsx
@@ -61,7 +61,11 @@ export const Library: React.FC<LibraryProps> = ({}) => {
                     <button
                         type="button"
                         className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                        onClick={() => {}}
+                        onClick={
+                            () => {
+                                m.modalService.fileUpload.open$.next(true)
+                            }
+                        }
                     >
                         Add Media Source
                     </button>
