43251cc8e5c8ed853e940cf9a3904f51b4719183
marvinirwin <marvin@marvinirwin.com>
2023-03-10 02:14:53 +0800

    lets try this proxy config

--- server/proxy.js
+++ server/proxy.js
@@ -5,25 +5,39 @@ const http = require('http');
 const proxy = httpProxy.createProxyServer();
 
 
+/*
 proxy.on(
     'proxyReq',
     (proxyReq, req, res, options) => {
-        // Add custom header to request
-        proxyReq.setHeader('Host', req.headers.host);
-        proxyReq.setHeader('X-Real-IP', req.socket.remoteAddress);
-        proxyReq.setHeader('X-Forwarded-For', req.headers['x-forwarded-for'] || '');
-        proxyReq.setHeader('X-Forwarded-Proto', req.headers['x-forwarded-proto'] || '');
-
     }
 )
+*/
 
 // Set up the server to handle requests
 const server = http.createServer((req, res) => {
     if (req.url.startsWith('/realms') || req.url.startsWith('/resources')) {
-        proxy.web(req, res, { target: 'http://localhost:8080' });
+        proxy.web(
+            req,
+            res,
+            {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                headers: {
+                    Host: req.headers.host,
+                    'X-Forwarded-For': req.connection.remoteAddress,
+                    'X-Forwarded-Host': req.headers.host,
+                    'X-Forwarded-Port': '443',
+                    'X-Forwarded-Proto': req.protocol,
+                    'X-Forwarded-Scheme': req.protocol,
+                    'X-Scheme': req.protocol,
+                    'X-Original-Forwarded-For': req.headers['x-forwarded-for'] || '',
+                    Proxy: ''
+                }
+            }
+        );
     } else {
         // Otherwise, proxy requests to localhost:3001
-        proxy.web(req, res, { target: 'http://localhost:3001' });
+        proxy.web(req, res, {target: 'http://localhost:3001'});
     }
 });
 
